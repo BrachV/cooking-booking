80170a72428e4cbc245621e7b8aeb12d8fb5838c
BrachV <v.brach57@gmail.com>
2024-02-22 21:05:09 +0100

    feat(front/app): apply page routing

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import { LandingPage } from './pages/LandingPage.tsx'
 import { AtelierPage } from './pages/AtelierPage.tsx'
+import { ApplyPage } from './pages/ApplyPage.tsx'
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/ateliers" element={<AtelierPage />} />
+      <Route path="/postuler" element={<ApplyPage />} />
     </Routes>
   )
 }
