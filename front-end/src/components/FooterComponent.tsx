cc5fff2f0c8759b5f03ed5db5db148ae607ad1a3
BrachV <v.brach57@gmail.com>
2024-02-22 21:05:36 +0100

    refactor(front/footerComponent): added padding

--- front-end/src/components/FooterComponent.tsx
+++ front-end/src/components/FooterComponent.tsx
@@ -1,8 +1,8 @@
 export const FooterComponent = () =>
 {
     return (
-        <div className="flex justify-center">
-            <p className="text-gray-800">Coooking-Booking@ Copyright 2024 </p>
+        <div className="flex justify-center p-6 bg-gray-50">
+            <p className="text-gray-800">Cooking-Booking@Copyright 2024 </p>
         </div>
     )
 }
