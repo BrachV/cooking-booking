db4a1e3c26ed2427f401a574b741fa5e02852f05
Maxime48 <maxime48@users.noreply.github.com>
2024-02-22 23:13:31 +0100

    Change type last hope

--- front-end/src/utils/types.ts
+++ front-end/src/utils/types.ts
@@ -30,43 +30,6 @@ export interface Wish {
     updatedAt?: Date;
 }
 
-//    {
-//         "id": 1,
-//         "utilisateur_email": "marie.curie@example.com",
-//         "atelierId": 1,
-//         "themeId": 1,
-//         "statut": "confirmé",
-//         "createdAt": "2024-02-22T19:11:32.000Z",
-//         "updatedAt": "2024-02-22T19:12:43.000Z",
-//         "atelier": {
-//             "id": 1,
-//             "nom": "Atelier Croissant",
-//             "capacite": 12,
-//             "date": "2023-12-12T09:00:00.000Z",
-//             "description": "Apprendre à faire les croissants parfaits.",
-//             "themeId": 1,
-//             "createdAt": "2024-02-22T19:07:28.000Z",
-//             "updatedAt": "2024-02-22T19:07:28.000Z",
-//             "theme": {
-//                 "id": 1,
-//                 "nom": "France",
-//                 "abreviation": "fr",
-//                 "description": "Explorer la riche cuisine française, de la baguette traditionnelle au coq au vin.",
-//                 "createdAt": "2024-02-22T19:05:04.000Z",
-//                 "updatedAt": "2024-02-22T19:05:04.000Z"
-//             }
-//         }
-//     }
-//export interface Participation {
-//     id: number;
-//     utilisateur_email: string;
-//     atelierId: number;
-//     themeId: number;
-//     statut: 'en_attente' | 'confirmé' | 'annulé';
-//     createdAt?: Date;
-//     updatedAt?: Date;
-// }
-
 export interface Participation {
     id: number;
     utilisateur_email: string;
