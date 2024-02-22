6fb4fb8bfdc567c01fbbdc4fa8df7f7c7f92340e
BrachV <v.brach57@gmail.com>
2024-02-22 19:59:06 +0100

    feat(front/app): ateliers route

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import { LandingPage } from './pages/LandingPage.tsx'
+import { AtelierPage } from './pages/AtelierPage.tsx'
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/ateliers" element={<AtelierPage />} />
     </Routes>
   )
 }
