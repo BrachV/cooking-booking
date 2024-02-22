578326541ebc19c85e05ece366b2791a713c6fd4
Maxime48 <maxime48@users.noreply.github.com>
2024-02-22 23:11:15 +0100

    Launch verif

--- front-end/src/pages/AdminPage.tsx
+++ front-end/src/pages/AdminPage.tsx
@@ -1,7 +1,16 @@
 import { FooterComponent } from "../components/FooterComponent.tsx"
 import { HeaderComponent } from "../components/HeaderComponent.tsx"
+import {Simulate} from "react-dom/test-utils";
+import submit = Simulate.submit;
+import {assignParticipants} from "../utils/api.js";
 
 export const AdminPage = () => {
+
+	//trier les ateliers call assignParticipants api
+	const trierAteliers = async () => {
+		await assignParticipants();
+	}
+
 	return (
 		<>
 			<div className="min-h-screen bg-gray-50 ">
@@ -14,7 +23,7 @@ export const AdminPage = () => {
 			</div> */}
 				<div className="flex flex-row justify-center items-center">
 					<a className="bg-blue-500 text-white p-2 rounded-lg m-2 w-52 text-center" href="/admin/ateliers">Voir les ateliers</a>
-					<button className="bg-blue-500 text-white p-2 rounded-lg m-2 w-52 text-center">Faire le tirage des souhaits</button>
+					<button onClick={trierAteliers} className="bg-blue-500 text-white p-2 rounded-lg m-2 w-52 text-center">Faire le tirage des souhaits</button>
 				</div>
 			</div>
 			<FooterComponent />
