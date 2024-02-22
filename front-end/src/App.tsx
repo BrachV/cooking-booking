efe00728fbc266db80e89e1d110aaa900b52d2a1
BrachV <v.brach57@gmail.com>
2024-02-22 21:36:33 +0100

    feat(front/app): routing for /profil page

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import { LandingPage } from './pages/LandingPage.tsx'
 import { AtelierPage } from './pages/AtelierPage.tsx'
 import { ApplyPage } from './pages/ApplyPage.tsx'
+import { ProfilePage } from './pages/ProfilePage.tsx'
 
 
 function App() {
@@ -11,6 +12,8 @@ function App() {
       <Route path="/" element={<LandingPage />} />
       <Route path="/ateliers" element={<AtelierPage />} />
       <Route path="/postuler" element={<ApplyPage />} />
+      <Route path="/profil" element={<ProfilePage />} />
+      <Route path="*" element={<h1>404 - Aucune page</h1>} />
     </Routes>
   )
 }
